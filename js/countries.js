5e79cef6f88300b8007752068717d474976bbd51
amanda maarman <amandamaarman19911@gmail.com>
2022-02-22 17:01:42 +0200

    changed input text field to be number and set max and min

--- js/countries.js
+++ js/countries.js
@@ -1,9 +1,11 @@
+const countryListElem = document.querySelector('.countryList');
+
 const countries = ["Argentina", "Brazil", "Chile", "Zambia", "Uganda", "Malawi", "Rwanda", "Ireland", "Switzerland"];
 
 const flags = ["🇦🇷", "🇧🇷", "🇨🇱", "🇿🇲", "🇺🇬", "🇲🇼", "🇷🇼", "🇮🇪", "🇨🇭"];
 
 //use map method to extract a list of flags
-const Flags = flags.map(function(chosedCountry){
+const Flags = countries.map(function(chosedCountry){
     console.log('country:'+ chosedCountry)
-    return chosedCountry.Flags;
+    countryListElem.innerHTML =  chosedCountry;
 })
